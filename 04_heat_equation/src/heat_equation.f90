90ed252ef95a6e7659687c1770dc2f16264f3726
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-22 08:18:54 +1000

    Solving heat equation

--- 04_heat_equation/src/heat_equation.f90
+++ 04_heat_equation/src/heat_equation.f90
@@ -3,6 +3,7 @@
 !
 module HeatEquation
 use Types, only: dp
+use Constants, only: pi
 implicit none
 private
 public :: solve_heat_equation
@@ -45,22 +46,35 @@ subroutine linspace(from, to, array)
 end subroutine
 
 subroutine solve_heat_equation()
-    real(dp) :: x0, x1, dx, t0, tf, dt, alpha, k
-    integer :: nx
+    real(dp) :: l, x0, x1, dx, t0, tf, dt, alpha, k
+    real(dp), allocatable :: data(:,:), x_points(:)
+    integer :: nx, nt, in
 
     k = 2.28e-5_dp
+    l = 1._dp
     x0 = 0._dp
-    x1 = 1._dp
+    x1 = x1 + l
     nx = 20
     dx = (x1 - x0) / nx
     alpha = 0.25_dp
     dt =  alpha * dx**2 / k
 
     t0 = 0
-    ! nt = 100
-    ! tf = t0 + nt * 
+    nt = 100
+    allocate(data(nx, nt))
+    allocate(x_points(nx))
+    call linspace(x0, x1, x_points)
+
+    ! Set intial conditions
+    data(:, 1) = sin(pi * x_points / l)
+
+    do in = 1, nt - 1
+
+    end do
 
     print *, 'dx=', dx, ' dt=', dt
+    print *, 'xpoints=', x_points
+    print *, 'data(:,1)=', data(:, 1)
 end subroutine
 
 end module HeatEquation
