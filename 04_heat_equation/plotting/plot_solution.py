0d14dca6af7d966342985ec9cea66579c1b15948
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-22 16:32:32 +1000

    Work

--- 04_heat_equation/plotting/plot_solution.py
+++ 04_heat_equation/plotting/plot_solution.py
@@ -5,6 +5,8 @@ import os
 from io import StringIO
 import pandas as pd
 from plot_utils import create_dir
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
 def plot_solution(plot_dir):
@@ -15,26 +17,22 @@ def plot_solution(plot_dir):
     # if data is None:
     #     return
 
-    df = pd.read_table("../heat_eqn_output.txt")
-    print(df.columns)
+    df = pd.read_table("../heat_eqn_output.txt", sep='\s+', header=None)
+    x_values = [df.values[0, 1:]]
+    t_values = np.transpose([df.values[1:, 0]])
+    temperatures = df.values[1:,1:]
+    print(x_values)
 
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(x_values, t_values, temperatures, cmap=cm.bone)
+    ax.set_xlabel("Length x [m]")
+    ax.set_ylabel("Time t [s]")
+    ax.set_zlabel("Temperature [T]")
+    ax.set_title("Z-component of curl of velocity")
+    # plt.savefig(filename)
+    plt.show()
 
-    # plt.plot(df['t'], df['x'], label='Approximation')
-
-    # exact_t = np.arange(0.0, t_end, 0.01)
-    # exact_x = np.cos(exact_t)
-
-    # plt.plot(exact_t, exact_x, label='Exact $x=\cos(t)$', linestyle='--')
-    # plt.title(r'Solution of $\ddot{x} + x = 0, x(0)=1, \dot{x}(0)=0$ for dt=' + f'{delta_t}')
-    # plt.xlabel('t')
-    # plt.ylabel(r'x')
-    # plt.legend()
-    # plt.grid()
-    # plt.tight_layout()
-    # plotfile = os.path.join(plot_dir, f"approx_vs_exact_dt_{delta_t}.pdf")
-    # plt.savefig(plotfile)
-    # plt.show()
-    a = 2
 
 
 if __name__ == '__main__':
