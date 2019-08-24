ac6e3d8d03aab2272a201206e3e842bd64f7c1f4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-25 09:52:12 +1000

    Plotting

--- 04_heat_equation/plotting/plot_solution.py
+++ 04_heat_equation/plotting/plot_solution.py
@@ -6,6 +6,27 @@ from solve_pde import solve_pde
 from mpl_toolkits.mplot3d import Axes3D
 
 
+def make_plot(data, title, plot_dir, plot_file_name):
+    x_values = data["x_values"]
+    t_values = data["t_values"]
+    temperatures = data["temperatures"]
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(x_values, t_values, temperatures, cmap=plt.cm.jet)
+    ax.set_xlabel("Length x [m]")
+    ax.set_ylabel("Time t [s]")
+    ax.set_zlabel("Temperature T [K]")
+    ax.set_title(title)
+    ax.view_init(30, 45)
+    plt.tight_layout()
+    pdf_file = os.path.join(plot_dir, plot_file_name)
+    # pdf_file = pdf_file.replace(".", "_")
+    # pdf_file = f"{pdf_file}.pdf"
+    plt.savefig(pdf_file)
+    plt.show()
+
+
 def plot_solution(plot_dir, nx, alpha, k, nt):
     create_dir(plot_dir)
 
@@ -18,43 +39,21 @@ def plot_solution(plot_dir, nx, alpha, k, nt):
     dt = alpha * dx**2 / k
 
     data = result["data"]
-    x_values = data["x_values"]
-    t_values = data["t_values"]
-    temperatures = data["temperatures"]
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(x_values, t_values, temperatures, cmap=plt.cm.jet)
-    ax.set_xlabel("Length x [m]")
-    ax.set_ylabel("Time t [s]")
-    ax.set_zlabel("Temperature T [K]")
-    ax.set_title(f"Solution of heat equation\ndx={dx:.2e} m, dt={dt:.2e} s, $\\alpha$={alpha:.2e}")
-    ax.view_init(30, 45)
-    plt.tight_layout()
-    pdf_file = os.path.join(plot_dir, f"solution_{alpha:.2e}.pdf")
-    plt.savefig(pdf_file)
-    plt.show()
+    title = f"Solution of heat equation\ndx={dx:.2e} m, dt={dt:.2e} s, $\\alpha$={alpha:.2e}"
+    plot_file_name = f"nx_{nx}_alpha_{alpha:.2f}_solution.pdf"
+    make_plot(data=data, title=title, plot_dir=plot_dir, plot_file_name=plot_file_name)
 
     # Plot errors
+    # -------------
 
     data = result["errors"]
-    x_values = data["x_values"]
-    t_values = data["t_values"]
-    temperatures = data["temperatures"]
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(x_values, t_values, temperatures, cmap=plt.cm.jet)
-    ax.set_xlabel("Length x [m]")
-    ax.set_ylabel("Time t [s]")
-    ax.set_zlabel("Temperature T [K]")
-    ax.set_title(f"Errors of the solution to the heat equation\ndx={dx:.2e} m, dt={dt:.2e} s, $\\alpha$={alpha:.2e}")
-    ax.view_init(30, 45)
-    plt.tight_layout()
-    pdf_file = os.path.join(plot_dir, f"errors_alpha_{alpha:.2e}.pdf")
-    plt.savefig(pdf_file)
-    plt.show()
+    title = f"Errors of the solution to the heat equation\ndx={dx:.2e} m, dt={dt:.2e} s, $\\alpha$={alpha:.2e}"
+    plot_file_name = f"nx_{nx}_alpha_{alpha:.2f}_errors.pdf"
+    make_plot(data=data, title=title, plot_dir=plot_dir, plot_file_name=plot_file_name)
 
 
 if __name__ == '__main__':
+    plot_solution(plot_dir="plots", nx=5, alpha=0.1, k=2.28e-5, nt=35)
     plot_solution(plot_dir="plots", nx=21, alpha=0.25, k=2.28e-5, nt=300)
+    plot_solution(plot_dir="plots", nx=31, alpha=0.5625, k=2.28e-5, nt=180)
