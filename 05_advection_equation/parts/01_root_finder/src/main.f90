0474fd50cfc23fb61b58e3e768106eae4fedc1aa
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-27 14:21:36 +1000

    Work

--- 05_advection_equation/parts/01_root_finder/src/main.f90
+++ 05_advection_equation/parts/01_root_finder/src/main.f90
@@ -1,8 +1,8 @@
 ! Contains the entry point to the program
 program Main
-use HeatEquation, only: read_settings_solve_and_create_output
+use RootFinder, only: read_settings_find_roots_and_print_output
 implicit none
 
-call read_settings_solve_and_create_output(silent=.false.)
+call read_settings_find_roots_and_print_output(silent=.false.)
 
 end program Main
