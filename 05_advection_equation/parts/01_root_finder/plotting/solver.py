a489ae6ccb6c341abd3f89d866fabb5afc8f861b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-29 07:33:53 +1000

    Work

--- 05_advection_equation/parts/01_root_finder/plotting/solver.py
+++ 05_advection_equation/parts/01_root_finder/plotting/solver.py
@@ -9,7 +9,8 @@ import array
 
 def read_solution_from_file(path_to_data):
     """
-    Read solution from a binary file.
+    Read solution from a binary file. Please refer to README.md
+    for description of the binary file format used here.
 
     Parameters
     ----------
@@ -22,69 +23,6 @@ def read_solution_from_file(path_to_data):
         (x, y, z) tuple
             x, y, and z values of the solution,
             where x and y and 1D arrays, and z is a 2D array.
-
-
-    The data format
-    -----------
-
-    Here is how data is stored in the binary file:
-
-    [x]
-    [
-        nx: number of x values
-        Size: 4 bytes
-        Type: signed int
-    ]
-    [x]
-
-    [x]
-    [
-        nt: number of t values
-        Size: 4 bytes
-        Type: Signed int
-    ]
-    [x]
-
-    [x]
-    [
-        x_values: array of x values
-        Size: nx * 8 bytes
-        Type: Array of double floats. Length nx.
-    ]
-    [x]
-
-    [x]
-    [
-        t_values: array of t values
-        Size: nt * 8 bytes
-        Type: Array of double floats. Length: tx.
-    ]
-    [x]
-
-    [x]
-    [
-        solution: a 2D array containing solution. These are velocity values
-        for x and t coordinates.
-
-        Size: nx * nt * 8 bytes
-        Type: 2D array of double floats. Length: nx * nt.
-    ]
-    [x]
-
-    Notes
-    ------
-
-    * Here [x] means 4-byte separator. This is added by Fortran's `write`
-    function. This separator is written before and after a data block,
-    and its value is the length in bytes of this block.
-
-    * `solution` is a 2D array saved as a sequence of double precision
-    float numbers in the column-major order. For example, for nx=3, nt=2,
-    the data will be saved as:
-
-        [1, 1] [2, 1] [3, 1] [1, 2] [2, 2] [3, 2],
-
-    where first index is x and second index is t.
     """
 
     data = open(path_to_data, "rb").read()
