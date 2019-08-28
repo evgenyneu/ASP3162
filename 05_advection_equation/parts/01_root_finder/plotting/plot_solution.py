8a91a17b9637c46f2cae205b287ed83902eb42ce
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-08-28 13:17:45 +1000

    Work

--- 05_advection_equation/parts/01_root_finder/plotting/plot_solution.py
+++ 05_advection_equation/parts/01_root_finder/plotting/plot_solution.py
@@ -15,35 +15,58 @@ import os
 from solver import solve_equation
 
 
-def plot(plot_dir, plot_file_name):
-    x, y, z = solve_equation()
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+def plot_3d(x, y, z, plot_dir, plot_file_name):
     x = [x]
     y = np.transpose([y])
 
-    print(f'x= {np.array(x).shape}')
-    print(f'y= {np.array(y).shape}')
-    print(f'z= {np.array(z).shape}')
-    print(f'x {len(x)} y {y.shape} z {z.shape}')
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
     ax.plot_surface(x, y, z, cmap=plt.cm.jet)
-    ax.set_xlabel("Position x [m]")
-    ax.set_ylabel("Time t [s]")
+    plt.xlabel("Position x [m]")
+    plt.ylabel("Time t [s]")
     ax.set_zlabel("Velocity v [m/s]")
 
-    ax.set_title(("Analytical solution of advection equation\n"
-                  "$v_t + vv_x$ with initial condition\n$v(x,0)=\\cos(x)$"))
+    plt.title(("Analytical solution of advection equation\n"
+               "$v_t + vv_x=0$ with initial condition\n$v(x,0)=\\cos(x)$ at different times"))
 
     ax.view_init(30, 45)
     ax.set_zlim(0, 1)
     ax.invert_xaxis()
     plt.tight_layout()
-    # create_dir(plot_dir)
-    # pdf_file = os.path.join(plot_dir, plot_file_name)
-    # plt.savefig(pdf_file)
+    create_dir(plot_dir)
+    pdf_file = os.path.join(plot_dir, plot_file_name)
+    plt.savefig(pdf_file)
     plt.show()
 
 
+def plot_2d(x, y, z, plot_dir, plot_file_name):
+    for iy, t in enumerate(y):
+        velocities = z[iy, :]
+        plt.plot(x, velocities, label=f't={t:.1f} s')
+
+    plt.xlabel("Position x [m]")
+    plt.ylabel("Velocity v [m/s]")
+    plt.title(("Analytical solution of advection equation\n"
+               "$v_t + vv_x=0$ with initial condition\n$v(x,0)=\\cos(x)$"))
+
+    plt.ylim(0, 1.1)
+    plt.legend()
+    plt.tight_layout()
+    create_dir(plot_dir)
+    pdf_file = os.path.join(plot_dir, plot_file_name)
+    plt.savefig(pdf_file)
+    plt.show()
+
+
+def make_plots():
+    x, y, z = solve_equation()
+
+    # plot_3d(x=x, y=y, z=z, plot_dir="plots",
+            # plot_file_name="advection_analytical_solution_3d.pdf")
+
+    plot_2d(x=x, y=y, z=z, plot_dir="plots",
+            plot_file_name="advection_analytical_solution_2d.pdf")
+
+
 if __name__ == '__main__':
-    plot(plot_dir="plots", plot_file_name="advection_exact_solution.pdf")
+    make_plots()
