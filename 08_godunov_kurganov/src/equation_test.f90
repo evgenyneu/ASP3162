a45b09485b0df8fbf75c27933d0d233a4da01cac
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-09-23 20:39:55 +1000

    Unit test

--- 08_godunov_kurganov/src/equation_test.f90
+++ 08_godunov_kurganov/src/equation_test.f90
@@ -23,7 +23,7 @@ subroutine remove_ghost_cells_test(failures)
 
     allocate(solution(1, 4, 3))
 
-    solution = reshape((/ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 /), &
+    solution = reshape([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], &
                        shape(solution))
 
 
--- 08_godunov_kurganov/src/physics_test.f90
+++ 08_godunov_kurganov/src/physics_test.f90
@@ -2,7 +2,9 @@ module PhysicsTest
 use Types, only: dp
 use AssertsTest, only: assert_true, assert_equal, assert_approx
 
-use Physics, only: many_primitive_vectors_to_state_vectors
+use Physics, only: many_primitive_vectors_to_state_vectors, &
+                   many_state_vectors_to_primitive
+
 implicit none
 private
 public physics_test_all
@@ -30,10 +32,51 @@ subroutine many_primitive_vectors_to_state_vectors_test(failures)
                        __FILE__, __LINE__, failures)
 end
 
+subroutine many_state_vectors_to_primitive_test(failures)
+    integer, intent(inout) :: failures
+    real(dp) :: state_vectors(1, 3, 2)
+    real(dp), allocatable :: primitive_vectors(:, :, :)
+
+    state_vectors = reshape([1, 2, 3, 4, 5, 6], shape(state_vectors))
+
+    call many_state_vectors_to_primitive(&
+        state_vectors=state_vectors, &
+        primitive_vectors=primitive_vectors)
+
+    call assert_equal(size(primitive_vectors, 1), 1, &
+                      __FILE__, __LINE__, failures)
+
+    call assert_equal(size(primitive_vectors, 2), 3, &
+                      __FILE__, __LINE__, failures)
+
+    call assert_equal(size(primitive_vectors, 3), 2, &
+                      __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 1, 1), 1.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 2, 1), 2.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 3, 1), 3.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 1, 2), 4.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 2, 2), 5.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+
+    call assert_approx(primitive_vectors(1, 3, 2), 6.0_dp, 1e-5_dp, &
+                       __FILE__, __LINE__, failures)
+end
+
+
 subroutine physics_test_all(failures)
     integer, intent(inout) :: failures
 
     call many_primitive_vectors_to_state_vectors_test(failures)
+    call many_state_vectors_to_primitive_test(failures)
 end
 
 end module PhysicsTest
