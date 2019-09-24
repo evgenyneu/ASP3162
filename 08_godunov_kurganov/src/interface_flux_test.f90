d9293f50f9ce4c4100f8c3d4a3ad96026d16892b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-09-24 16:31:46 +1000

    Godunov flux

--- 08_godunov_kurganov/src/interface_flux_test.f90
+++ 08_godunov_kurganov/src/interface_flux_test.f90
@@ -1,7 +1,8 @@
 module InterfaceFluxTest
 use Types, only: dp
 use AssertsTest, only: assert_approx
-use InterfaceFlux, only: godunov_flux
+use InterfaceFlux, only: godunov_flux, interface_flux
+use Settings, only: program_settings
 implicit none
 private
 public interface_flux_test_all
@@ -92,6 +93,46 @@ subroutine godunov_flux_test__uleft_smaller_uright__zero(failures)
         __FILE__, __LINE__, failures)
 end
 
+subroutine interface_flux_test__godunov(failures)
+    integer, intent(inout) :: failures
+    type(program_settings) :: options
+    real(dp) :: flux(1)
+
+    options%method = 'godunov'
+
+    call interface_flux(options=options, &
+        state_vector_left=[-310.1_dp], &
+        state_vector_right=[-10.2_dp], &
+        flux_left=[-1.1_dp], &
+        flux_right=[-1.2_dp], &
+        eigenvalue_left=3.1_dp, &
+        eigenvalue_right=0.1_dp, &
+        flux=flux)
+
+    call assert_approx(flux(1), -1.2_dp, 1e-13_dp, &
+        __FILE__, __LINE__, failures)
+end
+
+subroutine interface_flux_test__kurganov(failures)
+    integer, intent(inout) :: failures
+    type(program_settings) :: options
+    real(dp) :: flux(1)
+
+    options%method = 'kurganov'
+
+    call interface_flux(options=options, &
+        state_vector_left=[-310.1_dp], &
+        state_vector_right=[-10.2_dp], &
+        flux_left=[-1.1_dp], &
+        flux_right=[-1.2_dp], &
+        eigenvalue_left=3.1_dp, &
+        eigenvalue_right=0.1_dp, &
+        flux=flux)
+
+    call assert_approx(flux(1), -465.995_dp, 1e-13_dp, &
+        __FILE__, __LINE__, failures)
+end
+
 
 
 subroutine interface_flux_test_all(failures)
@@ -99,10 +140,12 @@ subroutine interface_flux_test_all(failures)
 
     call godunov_flux_test__uleft_greater_uright__speed_positive(failures)
     call godunov_flux_test__uleft_greater_uright__speed_negative(failures)
-
     call godunov_flux_test__uleft_smaller_uright__uleft_positive(failures)
     call godunov_flux_test__uleft_smaller_uright__uright_negative(failures)
     call godunov_flux_test__uleft_smaller_uright__zero(failures)
+
+    call interface_flux_test__godunov(failures)
+    call interface_flux_test__kurganov(failures)
 end
 
 end module InterfaceFluxTest
