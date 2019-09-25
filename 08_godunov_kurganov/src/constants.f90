4b85dd7b23f3df1adae9a1edc43d4974e21743f5
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-09-25 10:07:03 +1000

    Work

--- 08_godunov_kurganov/src/constants.f90
+++ 08_godunov_kurganov/src/constants.f90
@@ -5,9 +5,9 @@ use Types, only: dp
 implicit none
 private
 public pi, e, I
-! Constants contain more digits than double precision, so that
-! they are rounded correctly:
-real(dp), parameter :: pi   = 3.1415926535897932384626433832795_dp
-real(dp), parameter :: e    = 2.7182818284590452353602874713527_dp
-complex(dp), parameter :: I = (0, 1)
+    ! Constants contain more digits than double precision, so that
+    ! they are rounded correctly:
+    real(dp), parameter :: pi   = 3.1415926535897932384626433832795_dp
+    real(dp), parameter :: e    = 2.7182818284590452353602874713527_dp
+    complex(dp), parameter :: I = (0, 1)
 end module
