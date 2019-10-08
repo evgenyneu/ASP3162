11ab6f392f9c1477c15f592bea3e4823f218a730
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-10-09 08:35:36 +1100

    Improved Euler method

---
+++ 10_integration_python/improved_euler_integrator.py
@@ -0,0 +1,35 @@
+from step_integrate import StepIntegrate
+
+
+class ImprovedEulerIntegrator(StepIntegrate):
+    """Perform integration using improved Euler method"""
+
+    def advance(self, x, y):
+        """
+        Calcualte one step of integration using improved Euler method.
+
+        Parameters
+        ----------
+
+        x : float
+            Value of independent variable
+
+        y : numpy.ndarray
+            A 1D array containing dependent variable
+
+        Returns
+        -------
+
+        tuple
+
+        (x, y)
+
+        """
+
+        f = self.f
+        h = self.h
+        y_bar = y + h * f(x, y)
+        y = y + h * (f(x, y) + f(x + h, y_bar)) / 2
+        x += h
+
+        return x, y
